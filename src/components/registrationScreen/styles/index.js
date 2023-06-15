ffe6b5b8952235e3195e3556712e383d71703aeb
Sara Pereira <sara.pereira@uniphore.com>
2023-06-15 09:05:26 -0700

    registration screen

---
+++ src/components/registrationScreen/styles/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  errorMessage: {
+    color: "red",
+  },
+  passwordError: {
+    color: "red",
+    marginBottom: 5,
+  },
+});
