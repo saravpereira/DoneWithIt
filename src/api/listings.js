7174b26ee1d4ade6640cac2cc7ca766f253f7d68
Sara Pereira <77545921+saravpereira@users.noreply.github.com>
2023-07-24 21:28:27 -0700

    post api for listing (#13)

--- src/api/listings.js
+++ src/api/listings.js
@@ -4,6 +4,36 @@ const endpoint = "/listings";
 
 const getListings = () => client.get(endpoint);
 
+export const addListings = (listing, onUploadProgress) => {
+  const data = new FormData();
+
+  data.append("title", listing.name);
+  data.append("price", listing.price);
+  data.append("categoryId", listing.category.value);
+  data.append("description", listing.description);
+
+  listing.images.forEach((image, index) => {
+    data.append("images", {
+      name: `image${index}`,
+      type: "image/jpeg",
+      uri: image,
+    });
+  });
+
+  if (listing.location) {
+    data.append("location", JSON.stringify(listing.location));
+  }
+
+  return client.post(endpoint, data, {
+    onUploadProgress: (progress) =>
+      onUploadProgress(progress.loaded / progress.loaded),
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
+
 export default {
   getListings,
+  addListings,
 };
